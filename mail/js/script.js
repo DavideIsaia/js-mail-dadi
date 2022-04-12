1dc5dc3f4bb045a1844b278e311ccd3197027a59
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-12 13:18:30 +0200

    inserito prompt

---
+++ mail/js/script.js
@@ -0,0 +1,8 @@
+// chiedo all’utente la sua e-mail
+const userMail = prompt("Inserisci la tua email");
+console.log(userMail);
+
+// controllo che sia presente nell'array
+const eMail = [];
+
+// stampo un messaggio finale
