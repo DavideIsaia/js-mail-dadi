842419f0021606ac1898412d1f2b483f661828e9
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-12 14:36:23 +0200

    aggiunto calcolo random per il lancio dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,8 @@
+// generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+const userDice = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+console.log("Il dado del giocatore è", userDice);
+
+const pcDice = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+console.log("Il dado del computer è", pcDice);
+
+// stabilire il vincitore, in base a chi fa il punteggio più alto
