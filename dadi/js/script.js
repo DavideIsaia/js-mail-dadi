21cb2ffb959aa5e118852b093fc5beeaee2905c0
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-12 14:44:01 +0200

    aggiunte condizioni del gioco e risultato giocata

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -6,3 +6,14 @@ const pcDice = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 console.log("Il dado del computer è", pcDice);
 
 // stabilire il vincitore, in base a chi fa il punteggio più alto
+let result;
+if (userDice > pcDice) {
+    result = "Hai vinto!";
+} else if (userDice < pcDice) {
+    result = "Hai perso!";
+} else if (userDice == pcDice) {
+    result = "Pareggio!";
+}
+
+// stampo il risultato
+console.log(result);
